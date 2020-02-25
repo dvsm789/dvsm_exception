2631a0fc37587a4624920d004eb1849311570578
DVSM <51982263+dvsm789@users.noreply.github.com>
2020-02-25 22:15:10 +0530

    Add files via upload

---
+++ dvsm_exception/src/main/java/Maven_project/dvsm_exception/ConstCost/InvalidMaterialSelect.java
@@ -0,0 +1,7 @@
+package Maven_project.dvsm_exception.ConstCost;
+
+public class InvalidMaterialSelect extends  Exception{
+    public InvalidMaterialSelect(){
+        super("Invalid material selected ");
+    }
+}
---
+++ dvsm_exception/src/main/java/Maven_project/dvsm_exception/ConstCost/TypeOfMaterials.java
@@ -0,0 +1,7 @@
+package Maven_project.dvsm_exception.ConstCost;
+
+public enum TypeOfMaterials {
+    standard,
+    mediumStandard,
+    highStandard;
+}
